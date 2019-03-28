c5233cc063fc2185b3cf5d1e6cc60fecce1219d5
Kyle-2 <48459007+Kyle-2@users.noreply.github.com>
2019-03-28 16:51:27 -0700

    Cars Are Done
    Only Movement in level 2

--- Jocktopus_Frogger_V0.01/Bar.Benzvi.CS230Project5/Source/ItemMovement.cpp
+++ Jocktopus_Frogger_V0.01/Bar.Benzvi.CS230Project5/Source/ItemMovement.cpp
@@ -15,19 +15,23 @@ namespace Behaviors
 	ItemMovement::ItemMovement(float speed, bool left)
 		: Component("ItemMovement"), moveLeft(left), speed(speed)
 	{}
+	Component * ItemMovement::Clone() const
+	{
+		return new ItemMovement(*this);
+	}
 	void ItemMovement::Initialize()
 	{
 		transform = GetOwner()->GetComponent<Transform>();
 	}
-	void ItemMovement::Update()
+	void ItemMovement::Update(float dt)
 	{
 		if (moveLeft)
 		{
-			transform->SetTranslation(Vector2D(transform->GetTranslation().x - speed, transform->GetTranslation().y));
+			transform->SetTranslation(Vector2D(transform->GetTranslation().x + (speed * dt), transform->GetTranslation().y));
 		}
 		else
 		{
-			transform->SetTranslation(Vector2D(transform->GetTranslation().x + speed, transform->GetTranslation().y));
+			transform->SetTranslation(Vector2D(transform->GetTranslation().x - (speed * dt), transform->GetTranslation().y));
 		}
 	}
 	void ItemMovement::SetSpeed(float Speed)
