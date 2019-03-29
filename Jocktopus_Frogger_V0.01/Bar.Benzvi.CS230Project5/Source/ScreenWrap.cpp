faa1a6a13b04cb66ef6aa4204e8262d2b88cf761
Kyle-2 <48459007+Kyle-2@users.noreply.github.com>
2019-03-28 17:05:54 -0700

    ScreenWrap Fixed

--- Jocktopus_Frogger_V0.01/Bar.Benzvi.CS230Project5/Source/ScreenWrap.cpp
+++ Jocktopus_Frogger_V0.01/Bar.Benzvi.CS230Project5/Source/ScreenWrap.cpp
@@ -29,21 +29,21 @@ namespace Behaviors
 
 		BoundingRectangle dimensions = Graphics::GetInstance().GetScreenWorldDimensions();
 
-		if (transform->GetTranslation().y > dimensions.top + (transform->GetScale().y / 2) && physics->GetVelocity().y > 0)
+		if (transform->GetTranslation().y > dimensions.top + (transform->GetScale().y * 1.5f) && physics->GetVelocity().y >= 0)
 		{
-			transform->SetTranslation(Vector2D(transform->GetTranslation().x, dimensions.bottom - (transform->GetScale().y / 2)));
+			transform->SetTranslation(Vector2D(transform->GetTranslation().x, dimensions.bottom - (transform->GetScale().y * 1.5f)));
 		}
-		else if (transform->GetTranslation().y < dimensions.bottom - (transform->GetScale().y / 2) && physics->GetVelocity().y < 0)
+		else if (transform->GetTranslation().y < dimensions.bottom - (transform->GetScale().y * 1.5f) && physics->GetVelocity().y <= 0)
 		{
-			transform->SetTranslation(Vector2D(transform->GetTranslation().x, dimensions.top + (transform->GetScale().y / 2)));
+			transform->SetTranslation(Vector2D(transform->GetTranslation().x, dimensions.top + (transform->GetScale().y * 1.5f)));
 		}
-		else if (transform->GetTranslation().x < dimensions.left - (transform->GetScale().x / 2) && physics->GetVelocity().x < 0)
+		else if (transform->GetTranslation().x < dimensions.left - (transform->GetScale().x * 1.5f) && physics->GetVelocity().x <= 0)
 		{
-			transform->SetTranslation(Vector2D(dimensions.right + (transform->GetScale().x / 2), transform->GetTranslation().y));
+			transform->SetTranslation(Vector2D(dimensions.right + (transform->GetScale().x * 1.5f), transform->GetTranslation().y));
 		}
-		else if (transform->GetTranslation().x > dimensions.right + (transform->GetScale().x / 2) && physics->GetVelocity().x > 0)
+		else if (transform->GetTranslation().x > dimensions.right + (transform->GetScale().x * 1.5f) && physics->GetVelocity().x >= 0)
 		{
-			transform->SetTranslation(Vector2D(dimensions.left - (transform->GetScale().x / 2), transform->GetTranslation().y));
+			transform->SetTranslation(Vector2D(dimensions.left - (transform->GetScale().x * 1.5f), transform->GetTranslation().y));
 		}
 
 	}
